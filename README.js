1679447a0e4ecbf804726f6d39d06e14e68482b8
dedeesantos <andreoliveira0632@gmail.com>
2024-07-29 12:52:09 -0300

    Update README.js

--- README.js
+++ README.js
@@ -1,23 +1,5 @@
-<?php
+js```
+  
+const name = "teste"
 
-  class YnaraSingleton {
-
-    private static $instance;
-
-    private __construct () {
-      $this->call_me = 'Ynara Ventura';
-      $this->code_name = 'γᴧν';
-      $this->born_date = date('07-06-2002');
-      $this->skills = array_combine([∑, ∞], ['PHP', 'CSS', 'NextJS', 'React', 'JavaScript']);
-      $this->about = 'FullStack Developer';
-    }
-
-    public static function get_instance ($born_date) {
-     if (empty(self::$instance))
-        self::$instance = new YnaraSingleton();
-      // retorna a unica instancia
-      return self::instance;
-    }
-
-  }
-?>
+```
